82fd69b5f72e9dac0bbaa310d34eaa19d2bdd17e
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2023-01-14 09:58:34 +0100

    Podstawowa wersja aplikacji Spring Boot

---
+++ src/main/java/edu/logintegra/wsbfinalproject2022/WsbFinalProject2022Application.java
@@ -0,0 +1,13 @@
+package edu.logintegra.wsbfinalproject2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbFinalProject2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbFinalProject2022Application.class, args);
+    }
+
+}
