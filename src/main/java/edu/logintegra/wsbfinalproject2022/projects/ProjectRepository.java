127a7c608b1ff48cc4ac6251043d2e34bdbe7cde
Paweł Staniszewski <pawel.staniszewski@logintegra.com>
2023-01-14 12:32:16 +0100

    Wyświetlanie prostej listy wszystkich projektów
    Listę możemy zobaczyć pod adresem http://localhost:8080/projects.

---
+++ src/main/java/edu/logintegra/wsbfinalproject2022/projects/ProjectRepository.java
@@ -0,0 +1,6 @@
+package edu.logintegra.wsbfinalproject2022.projects;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
